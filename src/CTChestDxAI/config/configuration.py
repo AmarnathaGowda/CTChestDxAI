5f930ffa96157c2661707c803790717fe921c23b
AmarnathaGowda <amarnath1413@gmail.com>
2024-03-17 00:06:42 +0530

    model evaluation using MLflow is implemented

--- src/CTChestDxAI/config/configuration.py
+++ src/CTChestDxAI/config/configuration.py
@@ -3,8 +3,10 @@ from CTChestDxAI.constants import *
 from CTChestDxAI.utils.common import read_yaml, create_directories
 from CTChestDxAI.entity.config_entity import (DataIngestionConfig,
                                               PrepareBaseModelConfig,
-                                              TrainingConfig
+                                              TrainingConfig,
+                                              EvaluationConfig
                                                 )
+from CTChestDxAI import logger
 
 
 class ConfigurationManager:
@@ -78,3 +80,14 @@ class ConfigurationManager:
 
         return training_config
     
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+            mlflow_uri="https://dagshub.com/amarnath1413/CTChestDxAI.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
